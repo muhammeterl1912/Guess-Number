b19d71759300cf53d704ce91a37a9210681e231a
Muhammet Erol <muhammederol971@gmail.com>
2024-02-28 01:21:40 +0100

    better version of code.

--- game.js
+++ game.js
@@ -1,19 +1,29 @@
 let randomNumbers = Math.ceil(Math.random() * 25);
-// console.log(randomNumbers);
+console.log(randomNumbers);
 let message = document.querySelector(".msg");
+let guessSelector = document.querySelector(".guess");
 let score = 10;
 let duplicate = [];
 let topScore = localStorage.getItem("top-score") || 0;
 document.querySelector(".top-score").textContent = topScore;
 
 document.querySelector(".check").addEventListener("click", () => {
-  const guess = +document.querySelector(".guess").value;
+  check();
+});
 
+const check = () => {
+  const guess = +guessSelector.value;
+  const res = duplicateNumbers(guess);
+  if (res) {
+    return false;
+  }
+  // console.log(duplicate,res,guess)
+  duplicate.push(guess);
   if (!guess || guess < 0 || guess > 25) {
     message.textContent =
       "Please enter a valid number and a number between 1 and 25!!!";
-    document.querySelector(".guess").value = "";
-    document.querySelector(".guess").focus();
+    guessSelector.value = "";
+    guessSelector.focus();
   } else if (guess === randomNumbers) {
     message.textContent = "congratulations! You won the game.";
     document.querySelector("body").style.backgroundColor = "green";
@@ -34,21 +44,20 @@ document.querySelector(".check").addEventListener("click", () => {
       guess < randomNumbers
         ? (message.textContent = "Increase the number")
         : (message.textContent = "Decrease the number");
-      document.querySelector(".guess").value = "";
-      document.querySelector(".guess").focus();
+      guessSelector.value = "";
+      guessSelector.focus();
     } else {
       document.querySelector(".score").textContent = 0;
       document.querySelector("body").style.backgroundColor = "red";
       message.textContent = `Game Over!The Correct answer was ${randomNumbers}`;
     }
   }
-  duplicateNumbers(guess);
-});
+};
 
 const again = document.querySelector(".again");
 again.addEventListener("click", () => {
   document.querySelector("body").style.backgroundColor = "#2d3436";
-  randomNumbers = Math.ceil(Math.random() * 20);
+  randomNumbers = Math.ceil(Math.random() * 25);
   console.log(randomNumbers);
 
   score = 10;
@@ -57,25 +66,24 @@ again.addEventListener("click", () => {
 
   document.querySelector(".number").textContent = "?";
 
-  document.querySelector(".guess").value = "";
+  guessSelector.value = "";
 
-  message.textContent = "The game starts for new player... ";
+  message.textContent = "The game starts for the new player... ";
 
   document.querySelector(".check").disabled = false;
 });
 
 // Enter Button
 document.addEventListener("keydown", (e) => {
-  e.key === "Enter" && document.querySelector(".check").click();
+  e.key === "Enter" && check();
 });
 
 function duplicateNumbers(number) {
   if (duplicate.includes(number)) {
-    score++;
-    message.textContent = `You have already entered ${number} enter unique number.`;
-    document.querySelector(".guess").value = "";
-    document.querySelector(".guess").focus();
-    return;
+    message.textContent = `You have already entered ${number}.Please enter an unique number.`;
+    guessSelector.value = "";
+    guessSelector.focus();
+    return true;
   }
-  duplicate.push(number);
+  return false;
 }
